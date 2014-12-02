6aeed6ec2875258697ef6764232eadd5adea1f97
bellx2 <bellx2@gmail.com>
2014-12-02 22:57:35 +0900

    Initial Commit

---
+++ ConcentMeter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ConcentMeter
+//
+//  Created by bell on 2014/12/02.
+//  Copyright (c) 2014年 Addon Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
